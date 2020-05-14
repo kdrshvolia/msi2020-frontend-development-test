b205e1f18dc3f711f522f0eb6c2dea06bde12b1a
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-14 16:52:52 +0300

    Updates categories names to uppercase

--- src/components/CategoriesList/CategoriesList.js
+++ src/components/CategoriesList/CategoriesList.js
@@ -2,12 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CategoriesList = ({ categories, setCategory }) => {
-
   return (
     <div>
       {categories.length === 0
         ? null
-        : categories.map((category) => <div onClick={setCategory}>{category}</div>)}
+        : categories.map((category) => <div onClick={setCategory}>{category.toUpperCase()}</div>)}
     </div>
   );
 };
