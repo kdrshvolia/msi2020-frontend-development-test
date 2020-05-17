03dfee68419e10e9a0d2bd902642141b9e88b448
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-17 11:46:02 +0300

    Adds propTypes

--- src/components/SvgIcon/SvgIcon.js
+++ src/components/SvgIcon/SvgIcon.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const SvgIcon = ({ fill, width, height, viewBox = '0 0 20 17', path, onClick }) => (
   <svg onClick={onClick} width={width} height={height} viewBox={viewBox}>
     <path d={path} fill={fill} />
   </svg>
 );
+
+SvgIcon.propTypes = {
+  fill: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+  viewBox: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default SvgIcon;
