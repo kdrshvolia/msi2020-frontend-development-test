ba73100069c596f8fec3a22b3b53eeb5f1598486
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-17 12:09:21 +0300

    Adds prop chosenCategory

--- src/components/CategoriesList/CategoriesList.js
+++ src/components/CategoriesList/CategoriesList.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CategoriesListWrapper, CategoryItem } from './StyledComponents';
+import { CategoriesListWrapper } from './StyledComponents';
+import CategoryItem from '../CategoryItem/CategoryItem';
 
-const CategoriesList = ({ categories, setCategory }) => {
+const CategoriesList = ({ categories, setCategory, chosenCategory }) => {
+  const handleClick = (id) => {
+    setCategory(id);
+  };
+
   return (
     <CategoriesListWrapper>
       {categories.length === 0
         ? null
         : categories.map((category) => (
-            <CategoryItem onClick={setCategory}>{category.toUpperCase()}</CategoryItem>
+            <CategoryItem
+              onClick={handleClick}
+              isChosen={category === chosenCategory}
+              id={category}
+            >
+              {category.toUpperCase()}
+            </CategoryItem>
           ))}
     </CategoriesListWrapper>
   );
@@ -17,6 +28,7 @@ const CategoriesList = ({ categories, setCategory }) => {
 CategoriesList.propTypes = {
   categories: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
   setCategory: PropTypes.func.isRequired,
+  chosenCategory: PropTypes.string.isRequired,
 };
 
 export default CategoriesList;
