14f02c5663b26fecda093c40d3060d888ccda655
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-18 11:40:40 +0300

    Adds global styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 import { withBoard } from './hocs/withBoard/withBoard';
 import Board from './components/Board/Board';
+import { GlobalStyle } from './StyledComponents';
 
 function App() {
   const EnhancedBoard = withBoard(Board);
-  return <EnhancedBoard />;
+  return (
+    <>
+      <GlobalStyle />
+      <EnhancedBoard />
+    </>
+  );
 }
 
 export default App;
---
+++ src/StyledComponents.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+   font-family: Roboto;
+    font-style: normal;
+  font-weight: normal;
+  color: #333333;
+  }
+`;
+
+export { GlobalStyle };
