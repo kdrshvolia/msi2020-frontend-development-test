1b8ad0124cd68b1cbe906b3a9061d68255b299fe
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-21 11:30:30 +0300

    Removes unused imports

--- src/components/FavSideBar/FavSideBar.js
+++ src/components/FavSideBar/FavSideBar.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FavoritesList } from '../Board/StyledComponents';
-import BurgerBtn from '../../images/favorites-burger-btn.svg';
-import CrossBtn from '../../images/favorites-cross-btn.svg';
-import FavoritesHeader from '../FavoritesHeader/FavoritesHeader';
 import JokesList from '../JokesList/JokesList';
-import { HamburgerIcon, Header } from './StyledComponents';
 
 const FavSideBar = ({ favoritedJokes, toggleFav, isFavoritedList, isSideBarOpened, setOpened }) => {
-  const setOpen = () => {
-    setOpened();
-  };
   return (
     <aside>
-      <Header>
-        {' '}
-        <HamburgerIcon>
-          <img
-            className="icon"
-            src={isSideBarOpened ? CrossBtn : BurgerBtn}
-            alt=""
-            onClick={setOpen}
-          />
-        </HamburgerIcon>
-        <FavoritesHeader title="Favourite" />
-      </Header>
       <FavoritesList isSideBarOpened={isSideBarOpened}>
         {Object.keys(favoritedJokes).length === 0 ? null : (
           <JokesList
