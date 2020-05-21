c38846256a02be025f3f1bbb2bf13d2b4fa289c3
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-21 11:29:38 +0300

    Updates positioning for SideBarHeader

--- src/components/FavSideBar/StyledComponents.js
+++ src/components/FavSideBar/StyledComponents.js
@@ -1,1 +1,19 @@
-import styled from 'styled-components';const Header = styled.div`  display: flex;`;const HamburgerIcon = styled.div`  cursor: pointer;  margin-right: 10px;`;export { Header, HamburgerIcon };
+import styled from 'styled-components';
+
+const SideBarHeader = styled.div`
+  display: flex;
+  position: absolute;
+  right: 5%;
+  z-index: 20;
+  top: 40px;
+  @media (min-width: 992px) {
+    display: none;
+  }
+`;
+
+const HamburgerIcon = styled.div`
+  cursor: pointer;
+  margin-right: 10px;
+`;
+
+export { SideBarHeader, HamburgerIcon };
