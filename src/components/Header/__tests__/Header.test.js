486ff8e6e079d436a0bd3f125fcf8bdff1c7bc17
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-23 17:27:19 +0300

    Adds snapshot test

---
+++ src/components/Header/__tests__/Header.test.js
@@ -0,0 +1,1 @@
+import React from 'react';import renderer from 'react-test-renderer';import Header from '../Header';describe('SvgIcon', () => {  it('renders correctly', () => {    const rendered = renderer.create(<Header title="title" subtitle="subtitle" />);    expect(rendered.toJSON()).toMatchSnapshot();  });});
