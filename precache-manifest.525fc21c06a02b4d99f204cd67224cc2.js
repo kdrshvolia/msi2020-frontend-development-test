543090f31dfb89ecaa4e3a73102b061720310fe2
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-23 18:44:45 +0300

    Updates

---
+++ precache-manifest.525fc21c06a02b4d99f204cd67224cc2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f6f4e4e2a2c7c029e802d52f1dc620",
+    "url": "/msi2020-frontend-development-test/index.html"
+  },
+  {
+    "revision": "28376881c71d3484ed2c",
+    "url": "/msi2020-frontend-development-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d2ddff441a2d122b018f",
+    "url": "/msi2020-frontend-development-test/static/js/2.dd46eec6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/msi2020-frontend-development-test/static/js/2.dd46eec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28376881c71d3484ed2c",
+    "url": "/msi2020-frontend-development-test/static/js/main.b06d80bd.chunk.js"
+  },
+  {
+    "revision": "3eca96df8b275b6f8336",
+    "url": "/msi2020-frontend-development-test/static/js/runtime-main.9f8a03e5.js"
+  },
+  {
+    "revision": "ffa892cbe43b2f73c3e38071a387401c",
+    "url": "/msi2020-frontend-development-test/static/media/favorites-burger-btn.ffa892cb.svg"
+  },
+  {
+    "revision": "889f54a4e41027303666188c35a7977c",
+    "url": "/msi2020-frontend-development-test/static/media/favorites-cross-btn.889f54a4.svg"
+  },
+  {
+    "revision": "0f10cee4fb9784272c90da4cd403996d",
+    "url": "/msi2020-frontend-development-test/static/media/link.0f10cee4.svg"
+  },
+  {
+    "revision": "2ebc560c2f870b79ef38e82640706469",
+    "url": "/msi2020-frontend-development-test/static/media/static-logo-fav.2ebc560c.svg"
+  },
+  {
+    "revision": "ded38f2dafb2a305e0518724dacb5b2b",
+    "url": "/msi2020-frontend-development-test/static/media/static-logo.ded38f2d.svg"
+  }
+]);
