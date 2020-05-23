7ca97f9a0f2236fc383645c4cfbd61745943f068
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-23 17:10:24 +0300

    Adds snapshot test

--- src/components/Radio/__tests__/Radio.test.js
+++ src/components/Radio/__tests__/Radio.test.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 import Radio from '../Radio';
+
+
+const option = 'search';
+const isChanged = jest.fn();
 
 describe('Radio', () => {
   it('Radio changes', () => {
-    const option = 'search';
-    const isChanged = jest.fn();
     const wrapper = mount(<Radio handleChange={isChanged} option={option} />);
     const radio = wrapper.find('input[type="radio"]');
     radio.simulate('change');
     expect(isChanged).toBeCalledWith(option);
+  });
+  it('Renders correctly', () => {
+    const rendered = renderer.create(<Radio handleChange={isChanged} option={option} />);
+    expect(rendered).toMatchSnapshot();
   });
 });
