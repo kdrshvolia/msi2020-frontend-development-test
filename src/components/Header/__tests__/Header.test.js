6a9ac15cd88d0cb8c2b7cda4e4b17379d98399f6
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-23 17:28:39 +0300

    Changes param of describe

--- src/components/Header/__tests__/Header.test.js
+++ src/components/Header/__tests__/Header.test.js
@@ -1,1 +1,10 @@
-import React from 'react';import renderer from 'react-test-renderer';import Header from '../Header';describe('SvgIcon', () => {  it('renders correctly', () => {    const rendered = renderer.create(<Header title="title" subtitle="subtitle" />);    expect(rendered.toJSON()).toMatchSnapshot();  });});
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Header from '../Header';
+
+describe('Header', () => {
+  it('renders correctly', () => {
+    const rendered = renderer.create(<Header title="title" subtitle="subtitle" />);
+    expect(rendered.toJSON()).toMatchSnapshot();
+  });
+});
