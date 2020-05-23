e934b5891af0779be0307b93ad3f672d84a2211d
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-23 17:32:59 +0300

    Adds snapshot test

--- src/components/FavSideBar/__tests__/FavSideBar.test.js
+++ src/components/FavSideBar/__tests__/FavSideBar.test.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 import FavSideBar from '../FavSideBar';
+
+const isClicked = jest.fn();
+const toggleFav = jest.fn();
+const favorites = {
+  '1': {
+    id: '1',
+    url: 'url',
+    value: 'some value',
+    updated_at: 'date',
+    icon_url: 'url',
+    categories: ['category'],
+  },
+};
 
 describe('FavSideBar', () => {
   it('set opened on click', () => {
-    const isClicked = jest.fn();
-    const favorites = {
-      '1': {
-        id: '1',
-        url: 'url',
-        value: 'some value',
-        updated_at: 'date',
-        icon_url: 'url',
-        categories: ['category'],
-      },
-    };
     const wrapper = mount(<FavSideBar setOpened={isClicked} favoritedJokes={favorites} />);
-    const img = wrapper.find('img.icon');
+    const img = wrapper.find('img');
     img.simulate('click');
     expect(isClicked).toBeCalledWith();
+  });
+
+  it('renders correctly', () => {
+    const favJokes = {};
+    const rendered = renderer.create(
+      <FavSideBar
+        isSideBarOpened
+        setOpened={isClicked}
+        isFavoritedList
+        favoritedJokes={favJokes}
+        toggleFav={toggleFav}
+      />,
+    );
+    expect(rendered.toJSON()).toMatchSnapshot();
   });
 });
