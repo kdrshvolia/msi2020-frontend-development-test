5010c15881054523a2728e9f7ba896f6954fbe4d
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-23 16:49:56 +0300

    Adds snapshot test

--- src/components/SearchInput/__tests__/SearchInput.test.js
+++ src/components/SearchInput/__tests__/SearchInput.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 import SearchInput from '../SearchInput';
+
+const isChanged = jest.fn();
 
 describe('SearchInput', () => {
   it('Input text is changed', () => {
-    const isChanged = jest.fn();
     const wrapper = mount(<SearchInput onChange={isChanged} />);
     const input = wrapper.find('input');
     const event = {
@@ -14,5 +16,12 @@ describe('SearchInput', () => {
     };
     input.simulate('change', event);
     expect(isChanged).toBeCalledWith(event.target.value);
+  });
+
+  it('Renders correctly', () => {
+    const rendered = renderer.create(
+      <SearchInput inputText="abc" onChange={isChanged} placeholder="text" />,
+    );
+    expect(rendered).toMatchSnapshot();
   });
 });
