249eb8766b2a706d3555212b29cb269e70b17148
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-23 16:28:07 +0300

    Adds snapshot test

--- src/components/CategoryItem/__tests__/CategoryItem.test.js
+++ src/components/CategoryItem/__tests__/CategoryItem.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import renderer from 'react-test-renderer';
 import CategoryItem from '../CategoryItem';
 
 describe('CategoryItem', () => {
@@ -10,5 +11,13 @@ describe('CategoryItem', () => {
     const item = wrapper.find('div');
     item.simulate('click');
     expect(isClicked).toBeCalledWith(id);
+  });
+
+  it('renders correctly', () => {
+    const id = '1';
+    const isClicked = jest.fn();
+    const children = 'text';
+    const rendered = renderer.create(<CategoryItem onClick={isClicked} id={id} isChosen={false}>{children}</CategoryItem>);
+    expect(rendered.toJSON()).toMatchSnapshot();
   });
 });
