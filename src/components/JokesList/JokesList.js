18f0ac5967e574a218ed626ab6d3086df85e6dbc
Olia Kudriashova <oliakudriashova15@gmail.com>
2020-05-24 15:54:04 +0300

    Fixes order of jokes list

--- src/components/JokesList/JokesList.js
+++ src/components/JokesList/JokesList.js
@@ -1,51 +1,1 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import JokeContainer from '../JokeContainer/JokeContainer';
-import { List } from './StyledComponents';
-
-const JokesList = ({ jokesList, toggleFav, favoritedJokes, isFavoritedList }) => {
-  return (
-    <List isFavoritedList={isFavoritedList}>
-      {jokesList.reverse().map((joke) => {
-        const favorited = favoritedJokes ? !!favoritedJokes[joke.id] : true;
-        return (
-          <JokeContainer
-            joke={joke}
-            toggleFav={toggleFav}
-            isFavorited={favorited}
-            isInFavoritedList={isFavoritedList}
-          />
-        );
-      })}
-    </List>
-  );
-};
-
-JokesList.propTypes = {
-  jokesList: PropTypes.arrayOf(
-    PropTypes.shape({
-      categories: PropTypes.array,
-      created_at: PropTypes.string.isRequired,
-      icon_url: PropTypes.string.isRequired,
-      id: PropTypes.string.isRequired,
-      updated_at: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired,
-      value: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
-  toggleFav: PropTypes.func.isRequired,
-  favoritedJokes: PropTypes.shape({
-    id: PropTypes.shape({
-      categories: PropTypes.array,
-      created_at: PropTypes.string.isRequired,
-      icon_url: PropTypes.string.isRequired,
-      id: PropTypes.string.isRequired,
-      updated_at: PropTypes.string.isRequired,
-      url: PropTypes.string.isRequired,
-      value: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
-  isFavoritedList: PropTypes.bool.isRequired,
-};
-
-export default JokesList;
+import React from 'react';import PropTypes from 'prop-types';import JokeContainer from '../JokeContainer/JokeContainer';import { List } from './StyledComponents';const JokesList = ({ jokesList, toggleFav, favoritedJokes, isFavoritedList }) => {  return (    <List isFavoritedList={isFavoritedList}>      {jokesList.map((joke) => {        const favorited = favoritedJokes ? !!favoritedJokes[joke.id] : true;        return (          <JokeContainer            joke={joke}            toggleFav={toggleFav}            isFavorited={favorited}            isInFavoritedList={isFavoritedList}          />        );      })}    </List>  );};JokesList.propTypes = {  jokesList: PropTypes.arrayOf(    PropTypes.shape({      categories: PropTypes.array,      created_at: PropTypes.string.isRequired,      icon_url: PropTypes.string.isRequired,      id: PropTypes.string.isRequired,      updated_at: PropTypes.string.isRequired,      url: PropTypes.string.isRequired,      value: PropTypes.string.isRequired,    }),  ).isRequired,  toggleFav: PropTypes.func.isRequired,  favoritedJokes: PropTypes.shape({    id: PropTypes.shape({      categories: PropTypes.array,      created_at: PropTypes.string.isRequired,      icon_url: PropTypes.string.isRequired,      id: PropTypes.string.isRequired,      updated_at: PropTypes.string.isRequired,      url: PropTypes.string.isRequired,      value: PropTypes.string.isRequired,    }).isRequired,  }).isRequired,  isFavoritedList: PropTypes.bool.isRequired,};export default JokesList;
--- src/hocs/withBoard/withBoard.js
+++ src/hocs/withBoard/withBoard.js
@@ -1,146 +1,1 @@
-import React from 'react';
-import apiCall from '../../apiCall';
-
-const options = {
-  random: 'random',
-  categories: 'categories',
-  search: 'search',
-};
-
-const withBoard = (WrappedComponent) => {
-  return class extends React.Component {
-    state = {
-      chosenOption: options.random,
-      chosenCategory: '',
-      jokes: [],
-      favoritedJokes: JSON.parse(localStorage.getItem('favorites')) || {},
-      categories: [],
-      showCategories: 4,
-      inputText: '',
-      isSideBarOpened: false,
-    };
-
-    componentDidMount() {
-      this.getCategories();
-    }
-
-    toggleFav = (id) => {
-      const { jokes, favoritedJokes } = this.state;
-      const favJoke = jokes.find((joke) => joke.id === id);
-      if (favoritedJokes[id]) {
-        const { [id]: omit, ...updatedFavoritedJokes } = favoritedJokes;
-        this.setState(
-          {
-            favoritedJokes: updatedFavoritedJokes,
-          },
-          () => localStorage.setItem('favorites', JSON.stringify(this.state.favoritedJokes)),
-        );
-      } else {
-        this.setState(
-          (state) => {
-            return { favoritedJokes: { ...state.favoritedJokes, [id]: favJoke } };
-          },
-          () => localStorage.setItem('favorites', JSON.stringify(this.state.favoritedJokes)),
-        );
-      }
-    };
-
-    setType = (option) => {
-      this.setState({
-        chosenOption: option,
-      });
-    };
-
-    setCategory = (category) => {
-      this.setState({
-        chosenCategory: category,
-      });
-    };
-
-    setInputText = (text) => {
-      this.setState({
-        inputText: text,
-      });
-    };
-
-    setOpened = () => {
-      const { isSideBarOpened } = this.state;
-      this.setState({
-        isSideBarOpened: !isSideBarOpened,
-      });
-    };
-
-    getCategories = () => {
-      const { categories, showCategories } = this.state;
-      apiCall('https://api.chucknorris.io/jokes/categories').then((data) => {
-        this.setState(
-          {
-            categories: [...categories, ...data.slice(0, showCategories)],
-          },
-          () =>
-            this.setState({
-              chosenCategory: this.state.categories[0],
-            }),
-        );
-      });
-    };
-
-    getJoke = () => {
-      const { chosenOption, chosenCategory, jokes } = this.state;
-      if (chosenOption === options.categories) {
-        apiCall(`https://api.chucknorris.io/jokes/random?category=${chosenCategory}`).then(
-          (data) => {
-            this.setState({
-              jokes: [...jokes, data],
-            });
-          },
-        );
-      } else if (chosenOption === options.random) {
-        apiCall(`https://api.chucknorris.io/jokes/random`).then((data) => {
-          this.setState({
-            jokes: [...jokes, data],
-          });
-        });
-      } else if (chosenOption === options.search) {
-        const { inputText } = this.state;
-        apiCall(`https://api.chucknorris.io/jokes/search?query=${inputText}`).then((data) => {
-          this.setState({
-            jokes: [...jokes, ...data.result],
-          });
-        });
-      }
-    };
-
-    render() {
-      const {
-        chosenOption,
-        chosenCategory,
-        categories,
-        jokes,
-        favoritedJokes,
-        isSideBarOpened,
-        inputText,
-      } = this.state;
-      return (
-        <WrappedComponent
-          toggleFav={this.toggleFav}
-          inputText={inputText}
-          setInputText={this.setInputText}
-          options={options}
-          setType={this.setType}
-          setCategory={this.setCategory}
-          chosenOption={chosenOption}
-          chosenCategory={chosenCategory}
-          getJoke={this.getJoke}
-          jokes={jokes}
-          favoritedJokes={favoritedJokes}
-          categories={categories}
-          isSideBarOpened={isSideBarOpened}
-          setOpened={this.setOpened}
-        />
-      );
-    }
-  };
-};
-
-export { withBoard };
+import React from 'react';import apiCall from '../../apiCall';const options = {  random: 'random',  categories: 'categories',  search: 'search',};const withBoard = (WrappedComponent) => {  return class extends React.Component {    state = {      chosenOption: options.random,      chosenCategory: '',      jokes: [],      favoritedJokes: JSON.parse(localStorage.getItem('favorites')) || {},      categories: [],      showCategories: 4,      inputText: '',      isSideBarOpened: false,    };    componentDidMount() {      this.getCategories();    }    toggleFav = (id) => {      const { jokes, favoritedJokes } = this.state;      const favJoke = jokes.find((joke) => joke.id === id);      if (favoritedJokes[id]) {        const { [id]: omit, ...updatedFavoritedJokes } = favoritedJokes;        this.setState(          {            favoritedJokes: updatedFavoritedJokes,          },          () => localStorage.setItem('favorites', JSON.stringify(this.state.favoritedJokes)),        );      } else {        this.setState(          (state) => {            return { favoritedJokes: { ...state.favoritedJokes, [id]: favJoke } };          },          () => localStorage.setItem('favorites', JSON.stringify(this.state.favoritedJokes)),        );      }    };    setType = (option) => {      this.setState({        chosenOption: option,      });    };    setCategory = (category) => {      this.setState({        chosenCategory: category,      });    };    setInputText = (text) => {      this.setState({        inputText: text,      });    };    setOpened = () => {      const { isSideBarOpened } = this.state;      this.setState({        isSideBarOpened: !isSideBarOpened,      });    };    getCategories = () => {      const { categories, showCategories } = this.state;      apiCall('https://api.chucknorris.io/jokes/categories').then((data) => {        this.setState(          {            categories: [...categories, ...data.slice(0, showCategories)],          },          () =>            this.setState({              chosenCategory: this.state.categories[0],            }),        );      });    };    getJoke = () => {      const { chosenOption, chosenCategory, jokes } = this.state;      if (chosenOption === options.categories) {        apiCall(`https://api.chucknorris.io/jokes/random?category=${chosenCategory}`).then(          (data) => {            this.setState({              jokes: [data, ...jokes],            });          },        );      } else if (chosenOption === options.random) {        apiCall(`https://api.chucknorris.io/jokes/random`).then((data) => {          this.setState({            jokes: [data, ...jokes],          });        });      } else if (chosenOption === options.search) {        const { inputText } = this.state;        apiCall(`https://api.chucknorris.io/jokes/search?query=${inputText}`).then((data) => {          this.setState({            jokes: [...data.result, ...jokes],          });        });      }    };    render() {      const {        chosenOption,        chosenCategory,        categories,        jokes,        favoritedJokes,        isSideBarOpened,        inputText,      } = this.state;      return (        <WrappedComponent          toggleFav={this.toggleFav}          inputText={inputText}          setInputText={this.setInputText}          options={options}          setType={this.setType}          setCategory={this.setCategory}          chosenOption={chosenOption}          chosenCategory={chosenCategory}          getJoke={this.getJoke}          jokes={jokes}          favoritedJokes={favoritedJokes}          categories={categories}          isSideBarOpened={isSideBarOpened}          setOpened={this.setOpened}        />      );    }  };};export { withBoard };
